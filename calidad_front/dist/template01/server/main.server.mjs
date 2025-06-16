206eaf586f4d6a20afa0df9019d2b9d0c5e025fb
RichardFuentesTorreLinux <javierbaron6@gmail.com>
2025-06-15 23:46:03 -0500

    ajustes

--- calidad_front/dist/template01/server/main.server.mjs
+++ calidad_front/dist/template01/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BIU6WNFX.mjs";import"./chunk-LQM64AFB.mjs";import"./chunk-RCDNN43S.mjs";import"./chunk-P2LXTYIO.mjs";import"./chunk-ILRWNN2W.mjs";import"./chunk-T6JGTEUQ.mjs";import"./chunk-NW44QHZH.mjs";import"./chunk-7ZALS5IL.mjs";import"./chunk-COT65Y5O.mjs";export{a as default};
+import{a}from"./chunk-T4QN7AXK.mjs";import"./chunk-LQM64AFB.mjs";import"./chunk-RCDNN43S.mjs";import"./chunk-P2LXTYIO.mjs";import"./chunk-ILRWNN2W.mjs";import"./chunk-T6JGTEUQ.mjs";import"./chunk-NW44QHZH.mjs";import"./chunk-7ZALS5IL.mjs";import"./chunk-COT65Y5O.mjs";export{a as default};
